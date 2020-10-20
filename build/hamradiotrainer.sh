0bf7813b8ac909e9e243bd7fd0b87ac5eb54622e
Carsten Perthel <cpe@linuxcoder.de>
2020-10-20 19:27:29 +0200

    Use wget to workaround updater bug

--- build/hamradiotrainer.sh
+++ build/hamradiotrainer.sh
@@ -3,4 +3,7 @@
 # supress D-Bus fatal warnings
 export DBUS_FATAL_WARNINGS=0
 
+# Workaround for connection failure with built-in updater
+wget -N http://www.hamradiotrainer.de/download/de.amateurfunk.etq -O ~/HamRadioTrainer/data/de.amateurfunk.etq
+
 /usr/bin/wine ~/HamRadioTrainer/start-hamradiotrainer.exe
